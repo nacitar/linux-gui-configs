7bf0e254050401d2f34038abcd09ada6607eee0b
Jacob McIntosh <nacitar.sevaht@gmail.com>
2025-05-13 14:09:19 -0600

    avoid variable pollution

--- pc-specific/legion-7-16achg6/xprofile.d/10-office-screen-setup.sh
+++ pc-specific/legion-7-16achg6/xprofile.d/10-office-screen-setup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-
-screens=$(xrandr --query \
-    | sed -n 's/^\([^ ]\+\) connected .*$/\1/p' \
-    | sort \
-    | paste -sd ' ')
-if [[ ${screens} == "DP-2 DP-4 HDMI-0" ]]; then
+if [[ \
+        $(
+            xrandr --query \
+            | sed -n 's/^\([^ ]\+\) connected .*$/\1/p' \
+            | sort \
+            | paste -sd ' '
+        ) == "DP-2 DP-4 HDMI-0" \
+]]; then
     xrandr \
         --output HDMI-0 --mode 1920x1080 --pos 0x0 --primary \
         --output DP-2 --mode 1920x1080 --pos 1920x0 \
         --output DP-4 --off
 else
-    echo "Not applying configuration; unexpected screen setup: ${0}"
+    >&2 echo "WARNING: unexpected screen setup: ${screens}"
+    >&2 echo "- NOT APPLYING CONFIG: ${BASH_SOURCE[0]:-${0}}"
 fi
