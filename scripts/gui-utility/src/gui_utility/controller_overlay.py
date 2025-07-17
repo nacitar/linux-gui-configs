22418656c0b452f5d7f1e27e504876fded37b2bc
Jacob McIntosh <nacitar.sevaht@gmail.com>
2025-07-17 16:05:03 -0600

    Better controller tracking

--- scripts/gui-utility/src/gui_utility/controller_overlay.py
+++ scripts/gui-utility/src/gui_utility/controller_overlay.py
@@ -3,7 +3,9 @@ from __future__ import annotations
 import argparse
 import logging
 from dataclasses import dataclass, field
-from typing import Any, Optional, Sequence
+from enum import IntEnum, auto, unique
+from functools import cached_property
+from typing import Any, Generic, Optional, Protocol, Sequence, TypeVar
 
 from evdev import InputDevice, ecodes, list_devices
 
@@ -11,150 +13,185 @@ from .log_utility import add_log_arguments, configure_logging
 
 logger = logging.getLogger(__name__)
 
+T = TypeVar("T")
+
 
 @dataclass(frozen=True)
 class InputRange:
     minimum: int
     maximum: int
-    _minimum_pressed: int = field(init=False)
-    _delta: int = field(init=False)
+    flat: int
+    fuzz: int
 
-    def __post_init__(self) -> None:
-        object.__setattr__(  # because frozen
-            self, "_minimum_pressed", (self.minimum + self.maximum) // 2
-        )
-        object.__setattr__(  # because frozen
-            self, "_delta", (self.maximum - self.minimum)
-        )
+    @cached_property
+    def negative_flat(self) -> int:
+        return -self.flat
+
+    @cached_property
+    def half_maximum(self) -> int:
+        return self.maximum // 2
+
+    @cached_property
+    def half_minimum(self) -> int:
+        return self.minimum // 2
+
+    @cached_property
+    def abs_minimum(self) -> int:
+        return abs(self.minimum)
+
+    def clamp(self, value: int) -> int:
+        if value > self.negative_flat and value < self.flat:
+            return 0
+        if value < self.minimum:
+            return self.minimum
+        if value > self.maximum:
+            return self.maximum
+        return value
 
     def as_percentage(self, value: int) -> float:
-        return (value - self.minimum) / self._delta
+        if value < 0:
+            return value / self.abs_minimum
+        return value / self.maximum
 
     def is_pressed(self, value: int) -> bool:
-        return value > self._minimum_pressed
+        return value > self.half_maximum or value < self.half_minimum
 
 
-@dataclass(kw_only=True)
-class InputState:
+@dataclass
+class Profile(Generic[T]):
+    mapping: dict[str, T]
+
+
+@dataclass(kw_only=True, frozen=True)
+class Identifier(Generic[T]):
+    user: T | None
+    internal: str
+
+
+@dataclass(kw_only=True, frozen=True)
+class Input(Generic[T]):
+    identifier: Identifier[T]
     range: InputRange
-    value: int
 
-    def is_pressed(self) -> bool:
-        return self.range.is_pressed(self.value)
 
-    def __float__(self) -> float:
-        return self.range.as_percentage(self.value)
+class GamepadMonitor(Protocol[T]):
+    def sync_handler(self, controller: Controller[T]) -> bool: ...
+
+    def update_handler(
+        self, controller: Controller[T], input: Input[T], value: int
+    ) -> bool: ...
 
 
 @dataclass(kw_only=True)
-class Input:
-    name: str
-    state: InputState
+class Controller(Generic[T]):
+    device: InputDevice[Any]
+    profile: Profile[T] | None
 
+    forced_internal_names: dict[int, dict[int, str]] = field(
+        default_factory=dict
+    )
 
-class Controller:
-    PREFERRED_NAMES: set[str] = {
-        "BTN_SOUTH",
-        "BTN_EAST",
-        "BTN_NORTH",
-        "BTN_WEST",
-        "BTN_SELECT",
-        "BTN_START",
-        "BTN_MODE",
-        "BTN_TL",
-        "BTN_TR",
-        "BTN_THUMBL",
-        "BTN_THUMBR",
-        "ABS_X",
-        "ABS_Y",
-        "ABS_Z",
-        "ABS_RX",
-        "ABS_RY",
-        "ABS_RZ",
-        "ABS_HAT0X",
-        "ABS_HAT0Y",
-    }
+    inputs: dict[int, dict[int, Input[T]]] = field(
+        default_factory=dict, init=False
+    )
+    _last_raw_value: dict[int, dict[int, int]] = field(
+        default_factory=dict, init=False
+    )
 
-    # TODO: do something with this?
-    XBOX_MAPPING: dict[str, str] = {
-        "BTN_SOUTH": "A",
-        "BTN_EAST": "B",
-        "BTN_NORTH": "Y",
-        "BTN_WEST": "X",
-        "BTN_SELECT": "VIEW",  # or select?
-        "BTN_START": "MENU",  # or start?
-        "BTN_MODE": "XBOX",  # or guide?
-        "BTN_TL": "LB",
-        "BTN_TR": "RB",
-        "BTN_THUMBL": "LS",
-        "BTN_THUMBR": "RS",
-        "ABS_X": "LX",
-        "ABS_Y:": "LY",
-        "ABS_Z": "LT",
-        "ABS_RZ": "RT",
-        "ABS_RX": "RX",
-        "ABS_RY": "RY",
-        "ABS_HAT0X": "PadX",
-        "ABS_HAT0Y": "PadY",
-    }
-
-    @classmethod
-    def get_input_name(
-        cls, code: int, table: dict[int, str | tuple[str]]
+    def _get_internal_name(
+        self, code: int, table: dict[int, str | tuple[str]]
     ) -> str:
         names = table.get(code)
         if names:
             if isinstance(names, str):
                 names = (names,)
-            for name in names:
-                if name in cls.PREFERRED_NAMES:
-                    return name
-            logger.warning(f"No preferred name in list: {", ".join(names)}")
+            if self.profile:
+                for name in names:
+                    if name in self.profile.mapping:
+                        return name
+            logger.warning(
+                f"No profile-mapped names in list: {", ".join(names)}"
+            )
             return names[0]
         return ""
 
-    def __init__(self, device: InputDevice[Any]) -> None:
-        self.device = device
-        self.inputs: dict[int, dict[int, Input]] = {}
-
-        capabilities = device.capabilities(absinfo=False)
-
+    def __post_init__(self) -> None:
+        capabilities = self.device.capabilities(absinfo=False)
         for ev_type in [ecodes.EV_KEY, ecodes.EV_ABS]:
             for code in capabilities.get(ev_type, []):
+                name = self.forced_internal_names.get(ev_type, {}).get(
+                    code, ""
+                )
                 if ev_type == ecodes.EV_ABS:
-                    name = Controller.get_input_name(code, ecodes.ABS)
-                    raw_absinfo = device.absinfo(code)
-                    input_range = InputRange(raw_absinfo.min, raw_absinfo.max)
-                    value = raw_absinfo.value
+                    name = (
+                        name
+                        or self._get_internal_name(code, ecodes.ABS)
+                        or f"ABS_{code}"
+                    )
+                    raw_absinfo = self.device.absinfo(code)
+                    input_range = InputRange(
+                        minimum=raw_absinfo.min,
+                        maximum=raw_absinfo.max,
+                        flat=raw_absinfo.flat,
+                        fuzz=raw_absinfo.fuzz,
+                    )
                 else:
-                    name = Controller.get_input_name(
-                        code, ecodes.BTN
-                    ) or Controller.get_input_name(code, ecodes.KEY)
-                    input_range = InputRange(0, 1)
-                    value = int(code in device.active_keys(verbose=False))
+                    name = (
+                        name
+                        or self._get_internal_name(code, ecodes.BTN)
+                        or self._get_internal_name(code, ecodes.KEY)
+                        or f"KEY_{code}"
+                    )
+                    input_range = InputRange(
+                        minimum=0, maximum=1, flat=0, fuzz=0
+                    )
                 if not name:
                     raise AssertionError(
-                        f"Could not find name for input code: {code}"
+                        f"All inputs should be named by now but {code} isn't."
                     )
+                if self.profile:
+                    user_identifier = self.profile.mapping.get(name)
+                else:
+                    user_identifier = None
+
                 self.inputs.setdefault(ev_type, {})[code] = Input(
-                    name=name, state=InputState(range=input_range, value=value)
+                    identifier=Identifier[T](
+                        user=user_identifier, internal=name
+                    ),
+                    range=input_range,
                 )
 
-    def update(self, code: int, value: int, input_type: int) -> None:
-        input = self.inputs.get(input_type, {}).get(code, None)
-        if input:
-            input.state.value = value
-        else:
-            print(f"Skipping unknown input: {code}")
-
-    def __repr__(self) -> str:
-        return " ".join(
-            [
-                f"{input.name}={input.state.value}"
-                for input_type, code_to_input in self.inputs.items()
-                for input in code_to_input.values()
-            ]
-        )
+    def read_loop(self, monitor: GamepadMonitor[T]) -> None:
+        self._last_raw_value.clear()
+        updated = False
+        for event in self.device.read_loop():
+            if event.type in (ecodes.EV_KEY, ecodes.EV_ABS):
+                input = self.inputs.get(event.type, {}).get(event.code, None)
+                if input:
+                    event_type_section = self._last_raw_value.setdefault(
+                        event.type, {}
+                    )
+                    last_value = event_type_section.get(event.code)
+                    if last_value is not None:
+                        if abs(event.value - last_value) < input.range.fuzz:
+                            logger.debug(
+                                "Skipping update within fuzz range"
+                                f": {event.type}, {event.code}"
+                            )
+                            continue
+                    event_type_section[event.code] = event.value
+                    updated = True
+                    if not monitor.update_handler(self, input, event.value):
+                        break
+                else:
+                    logger.warning(
+                        f"Skipping unknown input: {event.type}, {event.code}"
+                    )
+            elif event.type == ecodes.EV_SYN:
+                if updated:
+                    updated = False
+                    if not monitor.sync_handler(self):
+                        break
 
 
 def find_controller(
@@ -167,8 +204,103 @@ def find_controller(
     return None
 
 
+@unique
+class GamepadInput(IntEnum):
+    A = auto()
+    B = auto()
+    X = auto()
+    Y = auto()
+    SELECT = auto()
+    START = auto()
+    GUIDE = auto()
+    LB = auto()
+    RB = auto()
+    LS = auto()
+    RS = auto()
+    LX = auto()
+    LY = auto()
+    RX = auto()
+    RY = auto()
+    LT = auto()
+    RT = auto()
+    DX = auto()
+    DY = auto()
+
+
+XINPUT_PROFILE = Profile[GamepadInput](
+    mapping={
+        "BTN_SOUTH": GamepadInput.A,
+        "BTN_EAST": GamepadInput.B,
+        "BTN_NORTH": GamepadInput.Y,
+        "BTN_WEST": GamepadInput.X,
+        "BTN_SELECT": GamepadInput.SELECT,
+        "BTN_START": GamepadInput.START,
+        "BTN_MODE": GamepadInput.GUIDE,
+        "BTN_TL": GamepadInput.LB,
+        "BTN_TR": GamepadInput.RB,
+        "BTN_THUMBL": GamepadInput.LS,
+        "BTN_THUMBR": GamepadInput.RS,
+        "ABS_X": GamepadInput.LX,
+        "ABS_Y": GamepadInput.LY,
+        "ABS_Z": GamepadInput.LT,
+        "ABS_RZ": GamepadInput.RT,
+        "ABS_RX": GamepadInput.RX,
+        "ABS_RY": GamepadInput.RY,
+        "ABS_HAT0X": GamepadInput.DX,
+        "ABS_HAT0Y": GamepadInput.DY,
+    }
+)
+
+
+@dataclass
+class GamepadInputState:
+    pressed: bool
+    value: float
+
+    def __str__(self) -> str:
+        if self.pressed:
+            return f"_{self.value}_"
+        return str(self.value)
+
+
+@dataclass
+class TerminalGamepadMonitor:
+    state: dict[GamepadInput, GamepadInputState] = field(default_factory=dict)
+
+    def sync_handler(self, controller: Controller[GamepadInput]) -> bool:
+        print(f"SYN: {self}")
+        return True
+
+    def update_handler(
+        self,
+        controller: Controller[GamepadInput],
+        input: Input[GamepadInput],
+        value: int,
+    ) -> bool:
+        if input.identifier.user:
+            value = input.range.clamp(value)
+            # not using setdefault to avoid constructing GamepadInputState
+            input_state = self.state.get(input.identifier.user)
+            if not input_state:
+                input_state = GamepadInputState(pressed=False, value=0)
+                self.state[input.identifier.user] = input_state
+            input_state.pressed = input.range.is_pressed(value)
+            input_state.value = input.range.as_percentage(value)
+            logger.debug(f"Update: {input.identifier.user} = {value}")
+        else:
+            logger.warning(f"UNHANDLED: {input.identifier.internal} = {value}")
+        return True
+
+    def __repr__(self) -> str:
+        return " ".join(
+            [f"{input.name}={value}" for input, value in self.state.items()]
+        )
+
+
 def main(argv: Sequence[str] | None = None) -> int:
-    parser = argparse.ArgumentParser(description="Monitors the battery status")
+    parser = argparse.ArgumentParser(
+        description="Monitors the state of a gamepad."
+    )
     add_log_arguments(parser)
     args = parser.parse_args(args=argv)
     configure_logging(args)
@@ -178,16 +310,11 @@ def main(argv: Sequence[str] | None = None) -> int:
         raise RuntimeError("Controller not found")
 
     print(f"Using device: {dev.path} ({dev.name})")
-    controller = Controller(dev)
+    controller = Controller(device=dev, profile=XINPUT_PROFILE)
+
+    monitor = TerminalGamepadMonitor()
     try:
-        while True:
-            print(controller)  # duplicated in syn case
-            for event in dev.read_loop():
-                if event.type in (ecodes.EV_KEY, ecodes.EV_ABS):
-                    controller.update(event.code, event.value, event.type)
-                elif event.type == ecodes.EV_SYN:
-                    break
-                    # so it can print the state again
+        controller.read_loop(monitor)
     except KeyboardInterrupt:
         print("\nExiting.")
     return 0
